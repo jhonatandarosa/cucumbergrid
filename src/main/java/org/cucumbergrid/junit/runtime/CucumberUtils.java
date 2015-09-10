5b89fd7592dc56903624ef2fe16a9b6a1a1bd81e
Jhonatan da Rosa <jhonatandarosa@gmail.com>
2015-09-10 09:25:19 -0300

    Resolves #8: NullPointerException after execution breaking tests

--- src/main/java/org/cucumbergrid/junit/runtime/CucumberUtils.java
+++ src/main/java/org/cucumbergrid/junit/runtime/CucumberUtils.java
@@ -1,28 +1,33 @@
 package org.cucumbergrid.junit.runtime;
 
+import java.lang.reflect.Field;
+
 import cucumber.runtime.model.CucumberFeature;
 import cucumber.runtime.model.CucumberScenario;
 import gherkin.formatter.model.Step;
-import org.junit.runner.Description;
-
 import java.io.Serializable;
-import java.lang.reflect.Field;
+import org.junit.runner.Description;
 
 public class CucumberUtils {
 
     private static Field fUniqueId;
+
+    private static String normalizeId(String id) {
+        return id.replaceAll("[^\\d\\w]", "");
+    }
+
     public static String getUniqueID(CucumberFeature feature) {
-        return feature.getGherkinFeature().getId();
+        return normalizeId(feature.getGherkinFeature().getId());
     }
 
     public static String getUniqueID(CucumberScenario cucumberScenario) {
-        return cucumberScenario.getGherkinModel().getId();
+        return normalizeId(cucumberScenario.getGherkinModel().getId());
     }
 
     public static String getUniqueID(CucumberScenario cucumberScenario, Step step) {
         String uniqueID = getUniqueID(cucumberScenario);
         uniqueID += ";" + step.getKeyword() + step.getName();
-        return uniqueID;
+        return normalizeId(uniqueID);
     }
 
     public static Serializable getDescriptionUniqueID(Description description) {
